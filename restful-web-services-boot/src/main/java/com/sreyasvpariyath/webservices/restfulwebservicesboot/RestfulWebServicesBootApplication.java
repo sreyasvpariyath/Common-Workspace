f53c1b04a921e63a44be692850b7effb179cdfa3
sreyasvpariyath <sreyas.vjayan@gmail.com>
2020-05-30 23:33:17 +0530

    Initial Commit

---
+++ restful-web-services-boot/src/main/java/com/sreyasvpariyath/webservices/restfulwebservicesboot/RestfulWebServicesBootApplication.java
@@ -0,0 +1,13 @@
+package com.sreyasvpariyath.webservices.restfulwebservicesboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesBootApplication.class, args);
+	}
+
+}
---
+++ restful-web-services-boot/src/test/java/com/sreyasvpariyath/webservices/restfulwebservicesboot/RestfulWebServicesBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyasvpariyath.webservices.restfulwebservicesboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
