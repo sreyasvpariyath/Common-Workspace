2e2c1ded13666aa40bd83da9c5da1c9a16a5c66e
sreyasvpariyath <sreyas.vjayan@gmail.com>
2020-06-01 14:19:26 +0530

    exception handler

--- restful-web-services-boot/src/main/java/com/sreyasvpariyath/webservices/restfulwebservicesboot/user/UserController.java
+++ restful-web-services-boot/src/main/java/com/sreyasvpariyath/webservices/restfulwebservicesboot/user/UserController.java
@@ -6,12 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.sreyasvpariyath.webservices.restfulwebservicesboot.exception.UserNotFoundException;
 
 @RestController
 @RequestMapping("demo/user")
@@ -39,6 +43,13 @@ public class UserController {
 		
 		return ResponseEntity.created(location).build();
 		
+	}
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		
+		if(user==null)
+			throw new UserNotFoundException("id-"+ id);		
 	}
 	
 }
